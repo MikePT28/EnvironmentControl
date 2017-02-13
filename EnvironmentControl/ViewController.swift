dc02910eabda0bcd10b71e398116268f040f729a
Mike Pesate <mike.pesate@mobgen.es>
2017-02-13 12:47:12 +0100

    Initial Commit

---
+++ EnvironmentControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnvironmentControl
+//
+//  Created by Mike on 13/02/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
